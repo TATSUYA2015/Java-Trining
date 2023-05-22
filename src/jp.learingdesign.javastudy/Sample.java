ac66ea1085c4ed5064060783d550d2cca902edfe
{若槻達也} <{t.w.dmm.study@gmail.com}>
2023-05-22 11:05:55 +0000

    更新

--- src/jp.learingdesign.javastudy/Sample.java
+++ src/jp.learingdesign.javastudy/Sample.java
@@ -2,6 +2,6 @@ package jp.learningdesign,javatudy;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello Tatsuya!");
+        System.out.println("Hello Sample Java!");
     }
 }
