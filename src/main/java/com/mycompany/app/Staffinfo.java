60f9072c715ef0a2eac9e48490880c9621695768
{若槻達也} <t.w.dmm.study@gmail.com>
2023-05-22 14:33:29 +0000

    新規学習　3-30

---
+++ src/main/java/com/mycompany/app/Staffinfo.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+class staff{
+    String name;
+    int staffid;
+    String email;
+}
+
+public class Staffinfo {
+
+    public static void name(String[] args) {
+        Staff yamada = new staff();
+        yamada.name = "Taro Yamada"
+
+        System.out.println(yamada.name);
+    }
+    
+}
