56a2b4b073369ba943b877600411a0113d8c81dc
{若槻達也} <{t.w.dmm.study@gmail.com}>
2023-05-22 11:33:43 +0000

    コメント練習

--- src/jp.learingdesign.javastudy/Sample.java
+++ src/jp.learingdesign.javastudy/Sample.java
@@ -1,7 +1,19 @@
 package jp.learningdesign,javatudy;
 
 public class Sample {
+
+    /*
+     * Sampleは、"Hello Sample Java!"と表示するプログラムです。
+     */
+
+    /**Javadoc形式
+     * author: Tatsuya Wakatsuki
+     * copyright:
+     * @param args
+     */
+
     public static void main(String[] args) {
+        //次の行でコンソールに文字列を出力します。
         System.out.println("Hello Sample Java!");
     }
 }
