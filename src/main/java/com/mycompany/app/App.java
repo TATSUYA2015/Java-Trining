74d1f6103359a42746d18d26e387fcfda0aa15bd
{若槻達也} <t.w.dmm.study@gmail.com>
2023-05-26 12:55:13 +0000

    docker 編集

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,26 @@
 
 package com.mycompany.app;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Java!");
+// public class App {
+//     public static void main(String[] args) {
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+
+class staff{
+    String name;
+    int staffid;
+    String email;
+}
+
+public class Staffinfo {
+
+    public static void name(String[] args) {
+        Staff yamada = new staff();
+        yamada.name = "Taro Yamada"
+
+        System.out.println(yamada.name);
     }
+    
 }
